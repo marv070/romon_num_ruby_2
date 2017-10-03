9af48e9674c9cef3c04e30861a8035b20bc6f947
Marvin Laucher <marv070@gmail.com>
2017-10-03 12:06:25 -0400

    update logic to include hash of key pairs of 5 and 1.

--- roman.rb
+++ roman.rb
@@ -1,3 +1,12 @@
 def converter(num)
-  "I"
+  pairs = {5 => "V", 1=> "I"}
+  results = ""
+  pairs.each do |number, roman|
+    div = num/number
+    div.times do 
+      results << roman
+      num-= number
+    end
+  end
+  results
 end
--- test_roman.rb
+++ test_roman.rb
@@ -3,8 +3,16 @@ require_relative "roman.rb"
 
 class Test_roman_numeral < Minitest::Test
 
-  def test_that_one_equals_I
+  def test_that_1_equals_I
     assert_equal("I", converter(1))
+  end
+
+  def test_that_5_returns_V
+    assert_equal("V", converter(5))
+  end
+
+  def test_that_3_returns_III
+    assert_equal("III", converter(3))
   end
 
 end
